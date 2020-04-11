bb618e84ee949b3823fa6e5385ef31f658825cb0
Sam Lakerveld <dark@dark.red>
2020-04-11 03:55:02 +0200

    add ssh

--- default.nix
+++ default.nix
@@ -94,6 +94,27 @@ in {
     };
   };
 
+  programs.ssh = {
+    enable = true;
+    controlMaster = "auto";
+    controlPersist = "10m";
+
+    extraConfig = ''
+      AddKeysToAgent true
+    '';
+
+    matchBlocks = {
+      "fubuki" = {
+        hostname = "dark.red";
+        forwardAgent = true;
+      };
+      "shinbuki" = {
+        hostname = "shin.dark.red";
+        forwardAgent = true;
+      };
+    };
+  };
+
   services.compton = {
     enable = true;
     backend = "glx";
